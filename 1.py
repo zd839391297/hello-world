ebf2af5abdf4e24ec89289deb560b33646b52759
张达 <34066746+zd839391297@users.noreply.github.com>
2018-11-14 13:33:11 +0800

    Create 1.py

---
+++ 1.py
@@ -0,0 +1,11 @@
+import requests
+from bs4 import BeautifulSoup
+
+n=1
+url="http://www.letpub.com.cn/index.php?journalid="+str(n)+"&page=journalapp&view=detail"
+code="utf-8"
+
+r=requests.get(url)
+r.raise_for_status()
+r.encoding=code
+print(r.text)
