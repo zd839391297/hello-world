71bbc03bca0e856f1807548233f0ae709712bf12
zd839391297 <zd839391297@outlook.com>
2018-11-19 19:22:42 +0800

    v3
    v3

--- 2.py
+++ 2.py
@@ -1,6 +1,7 @@
 import requests
 from bs4 import BeautifulSoup
 import xlrd
+import xlwt
 from xlutils.copy import copy
 
 oldWb = xlrd.open_workbook('test_xlwt.xls')
@@ -24,28 +25,29 @@ booksheet.write(0,10,'出版国家或地区')
 booksheet.write(0,11,'出版周期')
 booksheet.write(0,12, '出版年份')
 booksheet.write(0,13, '年文章数')'''
-for n in range(1320, 1400):
-    url = "http://www.letpub.com.cn/index.php?journalid=" + \
-        str(n)+"&page=journalapp&view=detail"
-    # code="utf-8"
-    r = requests.get(url)
-    # r.raise_for_status()
-    r.encoding = "UTF-8"
-    soup = BeautifulSoup(r.text, 'lxml')
-    soup_1_find = soup.body.div.next_sibling
-    for i in range(10):
-        soup_1_find = soup_1_find.next_sibling
-    soup_2_find = soup_1_find.div
-    for i in range(12):
-        soup_2_find = soup_2_find.next_sibling
-    soup_3_find = soup_2_find.div.h2
-    try:
+for i in range(80, 100):
+    for n in range(10*i, 10*i+10):
+        url = "http://www.letpub.com.cn/index.php?journalid=" + \
+            str(n)+"&page=journalapp&view=detail"
+        # code="utf-8"
+        r = requests.get(url)
+        # r.raise_for_status()
+        r.encoding = "UTF-8"
+        soup = BeautifulSoup(r.text, 'lxml')
+        try:
+            soup_1_find = soup.body.div.next_sibling
+        except:
+            n = n+1
+        for i in range(10):
+            soup_1_find = soup_1_find.next_sibling
+        soup_2_find = soup_1_find.div
+        for i in range(12):
+            soup_2_find = soup_2_find.next_sibling
+        soup_3_find = soup_2_find.div.h2
         for i in range(19):
             soup_3_find = soup_3_find.next_sibling
-    except AttributeError:
-        continue
-    soup_4_find = soup_3_find.tbody.tr.next_sibling
-    for i in range(14):
-        booksheet.write(n, i, soup_4_find.td.next_sibling.string)
-        soup_4_find = soup_4_find.next_sibling
-newWb.save("test_xlwt.xls")
+        soup_4_find = soup_3_find.tbody.tr.next_sibling
+        for i in range(14):
+            booksheet.write(n, i, soup_4_find.td.next_sibling.string)
+            soup_4_find = soup_4_find.next_sibling
+    newWb.save("test_xlwt.xls")
